50840265651a245cada49d2caab0fd02c3b7a2ed
Dan <=>
2022-12-07 11:21:25 +0700

    add text

--- src/pages/Book/Summary/Description/Description.js
+++ src/pages/Book/Summary/Description/Description.js
@@ -8,18 +8,14 @@ const Description = () => {
       <DescriptionItem title="Vật nuôi" text="Mèo" />
       <DescriptionItem
         title="Triệu chứng"
-        text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus."
+        text="Hay ho khan, ăn ít, đi phân lỏng. Lông rụng nhiều và hay gào mỗi đêm."
       />
-      <DescriptionItem title="Phòng khám" text="Lorem ipsum dolor" />
+      <DescriptionItem title="Phòng khám" text="Family Bean" />
       <DescriptionItem title="Bác sĩ" text="Trần Văn B" />
       <DescriptionItem title="Thời gian" text="16:00 13/11/2022" />
       <DescriptionItem
         title="Chú thích"
-        text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus."
+        text="Bé đã từng bị kí sinh trùng máu, dị ứng kháng sinh, đã triệt sản."
       />
     </div>
   );
